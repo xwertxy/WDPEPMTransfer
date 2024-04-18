60139ae09aa27f09ed3cde7fe6ce8dcd1928098a
Ace Sobremont <136815140144@ncr2.deped.gov.ph>
2024-04-18 21:19:06 +0800

    WdpeTransfer

--- src/xwertxy/WDPEPMTransfer/WdpeTransfer.php
+++ src/xwertxy/WDPEPMTransfer/WdpeTransfer.php
@@ -5,9 +5,15 @@ namespace xwertxy\WDPEPMTransfer;
 use pocketmine\player\Player;
 use pocketmine\plugin\PluginBase;
 use pocketmine\network\mcpe\protocol\TransferPacket;
+use pocketmine\utils\SingletonTrait;
 
 class WdpeTransfer extends PluginBase {
-
+  use SingletonTrait;
+  
+  public function onLoad() : void {
+    self::setInstance($this);
+  }
+  
   public function onEnable() : void {
     $this->getServer()->getCommandMap()->register("WDPEPMTransfer", new WdpeTransferCommand());
   }
